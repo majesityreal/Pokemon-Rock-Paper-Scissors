0fcf8b1565c3fbb43fda9aa9ceec977b25bc1993
majesityreal <carsonjanderson2020@gmail.com>
2024-04-03 16:43:34 -0400

    Added socket for createGame and joinGame.

--- client/client.js
+++ client/client.js
@@ -1,3 +1,12 @@
 console.log('client.js executing');
 
 var socket = io();
+
+function createGame() {
+    socket.emit('createGame')
+}
+
+function joinGame() {
+    roomUniqueId = document.getElementById('roomUniqueId').value;
+    socket.emit('joinGame', {roomUniqueId: roomUniqueId})
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server);
 
+const rooms = {}; // these are the rooms that currently have games open
 
 // Serve static files from the 'client' directory
 app.use(express.static(path.join(__dirname, 'client')));
@@ -22,8 +23,42 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user has connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    })
+
+    socket.on('createGame', () => {
+      const roomUniqueId = makeid(10);
+      rooms[roomUniqueId] = {};
+      socket.join(roomUniqueId); // connect incoming client (socket) to this room (by roomUniqueId)
+      socket.emit("newGame", {roomUniqueId: roomUniqueId}); // server returning newGame with data
+    })
+
+    socket.on('joinGame', (data) => {
+      if (rooms[data.roomUniqueId] != null) {
+        socket.join(data.roomUniqueId); // joining incoming request to the same room (roomUniqueId should already exist)
+        socket.to(data.roomUniqueId).emit('playersConnected', {}); // when t he two players join we can say they are connected
+        socket.emit("playersConnected");
+      }
+      const roomUniqueId = makeid(10);
+      rooms[roomUniqueId] = {};
+      socket.join(roomUniqueId); // connect incoming client (socket) to this room (by roomUniqueId)
+      socket.emit("newGame", {roomUniqueId: roomUniqueId}); // server returning newGame with data
+    })
+
 })
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+// helper function to make a room id
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
